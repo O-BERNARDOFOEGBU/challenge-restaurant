5a2e43cc80c9a6d241db86a87c68d537d00fad1c
Favour Afolayan <favourafolayan@gmail.com>
2021-05-16 18:42:45 +0100

    add loader to restaurants page

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -1,22 +1,26 @@
 <template>
   <div class="page page__stores">
     <div class="pages__stores-list">
-      <StoreList :stores="stores" />
+      <Loader v-if="loading" class="loader" />
+      <StoreList :stores="stores" v-else />
     </div>
   </div>
 </template>
 
 <script>
 import StoreList from '@/components/StoreList/StoreList';
+import Loader from '@/components/Loader/Loader';
 
 export default {
   name: 'Stores',
   components: {
     StoreList,
+    Loader,
   },
   data() {
     return {
       stores: [],
+      loading: true,
     };
   },
 
@@ -25,7 +29,22 @@ export default {
       .then((response) => response.json())
       .then((response) => {
         this.stores = response;
+        this.loading = false;
+      })
+      .catch(() => {
+        this.loading = false;
       });
   },
 };
 </script>
+
+<style lang="scss">
+.pages__stores-list {
+  display: flex;
+  justify-content: center;
+}
+
+.loader {
+  fill: var(--text-color);
+}
+</style>
